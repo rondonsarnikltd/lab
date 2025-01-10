e385daad974f87000228b597eb4e3969e5b8583d
lgcanetti <lgcanetti@gmail.com>
2025-01-10 18:26:06 -0500

    Personal file

---
+++ development/lgcanetti.clj
@@ -0,0 +1,16 @@
+(ns lgcanetti)
+
+(defn greet [name]
+    (str "Hello, " name "!"))
+
+(defn add [a b]
+    (+ a b))
+
+(defn subtract [a b]
+    (- a b))
+
+(defn multiply [a b]
+    (* a b))
+
+(defn divide [a b]
+    (/ a b))
