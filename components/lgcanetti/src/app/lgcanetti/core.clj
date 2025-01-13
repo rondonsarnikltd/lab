73ff6774100f5f589b8670c7513f999cfaed1f19
Yunior Alayo <yunior.alayo@gmail.com>
2025-01-13 23:20:16 +0100

    Added an example of handling a get request.

--- components/lgcanetti/src/app/lgcanetti/core.clj
+++ components/lgcanetti/src/app/lgcanetti/core.clj
@@ -1,5 +1,6 @@
 (ns app.lgcanetti.core
   (:require [hiccup2.core :as h]
+            [io.pedestal.http.params :as params] ;; we need this to access the path params in the requests
             [app.lgcanetti.index :as index]))
 
 ; Prepare the hiccup to return it as html
@@ -29,5 +30,19 @@
    :enter (fn [context]
             (assoc context :response (respond index/content)))})
 
+(def message-handler
+  {:name ::get
+   :enter (fn [context]
+            (let [message (-> context :request :path-params :message)]
+              ;; print the path param value in the terminal
+              (println message)
+              ;; Respond with the value of message 
+              (assoc context :response {:status 200
+                                        :headers {"Content-Type" "text/html" "Content-Security-Policy" "img-src 'self'"}
+                                        :body message})))})
+
 (def routes
-  #{["/lgcanetti" :get lgcanetti-page-handler :route-name ::lgcanetti-page]})
+  #{["/lgcanetti" :get lgcanetti-page-handler :route-name ::lgcanetti-page]
+    ["/lgcanetti/:message"
+     :get [params/keyword-params message-handler]
+     :route-name ::message-handler]})
