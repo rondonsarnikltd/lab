062fc6565e42dc3c5e4bbe3eaee4d206b2922003
lgcanetti <lgcanetti@gmail.com>
2025-01-14 15:55:41 -0500

    Raise an alert on browser when click on Team. Go to a /lgcanetti/:message route whe click the link Project on lgcanetti main page

--- components/lgcanetti/src/app/lgcanetti/index.clj
+++ components/lgcanetti/src/app/lgcanetti/index.clj
@@ -37,7 +37,7 @@
            "rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"}
           "Team"] 
          [:a
-          {:href "#",
+          {:href "/lgcanetti/Projects",
            :class
            "rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"}
           "Projects"]
