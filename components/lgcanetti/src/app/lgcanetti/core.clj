b57a617bc0161dcfe8856503c36ef2b84cf4a951
lgcanetti <lgcanetti@gmail.com>
2025-01-21 17:15:38 -0500

    Add login

--- components/lgcanetti/src/app/lgcanetti/core.clj
+++ components/lgcanetti/src/app/lgcanetti/core.clj
@@ -1,61 +1,70 @@
 (ns app.lgcanetti.core
-  (:require [hiccup2.core :as h]
-            [io.pedestal.http.params :as params] ;; we need this to access the path params in the requests
-            [app.lgcanetti.index :as index]))
+   (:require [hiccup2.core :as h]
+             [io.pedestal.http.params :as params] ;; we need this to access the path params in the requests
+             [app.lgcanetti.index :as index]
+             [app.lgcanetti.login :as login]))
 
 ; Prepare the hiccup to return it as html
-(defn template [html-body]
-  [:html
-   {:class "h-full bg-gray-100"}
-   [:head
-    [:meta
-     {:name "viewport" :content "width=device-width,initial-scale=1"}]
-    [:title "Luis G. Canetti Morales's Page"]
-    [:link {:href "tailwind.min.css" :rel "stylesheet"}]
-    [:script {:src "htmx.min.js"}]]
-   [:body
-    {:class "h-full"}
-    (h/raw html-body)]])
+ (defn template [html-body]
+   [:html
+    {:class "h-full bg-gray-100"}
+    [:head
+     [:meta
+      {:name "viewport" :content "width=device-width,initial-scale=1"}]
+     [:title "Luis G. Canetti Morales's Page"]
+     [:link {:href "tailwind.min.css" :rel "stylesheet"}]
+     [:script {:src "htmx.min.js"}]]
+    [:body
+     {:class "h-full"}
+     (h/raw html-body)]])
 
-(defn getHeaders []
-  {"Content-Type" "text/html"
-   "Content-Security-Policy" "img-src *"});Changed: "img-src 'self'" by "img-src *"
+ (defn getHeaders []
+   {"Content-Type" "text/html"
+    "Content-Security-Policy" "img-src *"});Changed: "img-src 'self'" by "img-src *"
 
-(defn ok [body]
-  {:status 200
-   :headers (getHeaders)
-   :body (-> body (h/html) (str))})
+ (defn ok [body]
+   {:status 200
+    :headers (getHeaders)
+    :body (-> body (h/html) (str))})
 
-(defn respond [content]
-  (ok (template (str (h/html (content))))))
+ (defn respond [content]
+   (ok (template (str (h/html (content))))))
 
-(defn respond-with-params [content value]
-  (ok (template (str (h/html (content value))))))
+ (defn respond-with-params [content value]
+   (ok (template (str (h/html (content value))))))
 
-(def lgcanetti-page-handler
-  {:name :get
-   :enter (fn [context]
-            (assoc context :response (respond index/content)))})
+ (def lgcanetti-page-handler
+   {:name :get
+    :enter (fn [context]
+             (assoc context :response (respond index/content)))})
 
-(defn processMsg [msg]
-  (let [args {:element [:p (str "The user has clicked on " msg)] :prod true}]
-    (respond-with-params index/content args)))
+ (defn processMsg [msg]
+   (let [args {:element [:p (str "The user has clicked on " msg)] :prod true}]
+     (respond-with-params index/content args)))
 
 
-  (def message-handler
-    {:name :get
-     :enter (fn [context]
-              (let [message (-> context :request :path-params :message)]
+ (def message-handler
+   {:name :get
+    :enter (fn [context]
+             (let [message (-> context :request :path-params :message)]
               ;; Respond with the value of message 
-                (assoc context :response (processMsg message))))})
+               (assoc context :response (processMsg message))))})
 
-  (def routes
-    #{["/lgcanetti"
-       :get lgcanetti-page-handler
-       :route-name ::lgcanetti-page]
-      ["/lgcanetti/"
-       :get lgcanetti-page-handler
-       :route-name ::lgcanetti-prod]
-      ["/lgcanetti/:message";TODO This route do not loads stylesheets neither scripts (Review Caddy config with Junior)
-       :get [params/keyword-params message-handler]
-       :route-name ::message-handler]})
+ (def lgcanetti-login-handler
+   {:name :get
+    :enter (fn [context]
+             (assoc context :response (respond login/login-page)))})
+
+(def routes
+  #{["/lgcanetti"
+     :get lgcanetti-page-handler
+     :route-name ::lgcanetti-page]
+    ["/lgcanetti/"
+     :get lgcanetti-page-handler
+     :route-name ::lgcanetti-prod]
+    ["/lgcanetti/nav/:message";TODO This route do not loads stylesheets neither scripts (Review Caddy config with Yunior)
+     :get [params/keyword-params message-handler]
+     :route-name ::message-handler]
+    ["/lgcanetti/login"
+     :get lgcanetti-login-handler
+     :route-name ::lgcanetti-login]})
