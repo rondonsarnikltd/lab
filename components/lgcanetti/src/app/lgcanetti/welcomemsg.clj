9ea8485e24e9c4827ab4608af96c52aaec8e627c
lgcanetti <lgcanetti@gmail.com>
2025-01-23 19:09:16 -0500

    Welcome text rectification

--- components/lgcanetti/src/app/lgcanetti/welcomemsg.clj
+++ components/lgcanetti/src/app/lgcanetti/welcomemsg.clj
@@ -3,4 +3,5 @@
 (defn welcome-msg []
   [:div
    [:h1 "Welcome to my page!"]
-   [:p "This is a Tailwind Dasboard template, may not function well due version incompatibilities..."]])
+   [:p "This is a Tailwind Dasboard template, may not function well due need javascript code too handle dropdown menu that is not implemented here."]
+   ])
