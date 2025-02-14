0c5793ba131d849a6ae25904417ec31cf60b7f16
Yunior Alayo <yunior.alayo@gmail.com>
2025-02-14 11:14:41 +0100

    Added mp3 players for the songs.

--- components/dylan/src/app/dylan/index.clj
+++ components/dylan/src/app/dylan/index.clj
@@ -6,18 +6,13 @@
    [:header
     {:class "absolute inset-x-0 top-0 z-50"}
     [:nav
-     {:class "flex items-center justify-between p-6 lg:px-8",
+     {:class "flex items-center justify-between p-6 lg:px-8 lg:hidden",
       :aria-label "Global"}
      [:div
       {:class "flex lg:flex-1"}
       [:a
        {:href "#", :class "-m-1.5 p-1.5"}
-       [:span {:class "sr-only"} "Your Company"]
-       [:img
-        {:class "h-8 w-auto",
-         :src
-         "https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600",
-         :alt ""}]]]
+       [:span {:class "sr-only"} "Your Company"]]]
      [:div
       {:class "flex lg:hidden"}
       [:button
@@ -70,12 +65,7 @@
        {:class "flex items-center justify-between"}
        [:a
         {:href "#", :class "-m-1.5 p-1.5"}
-        [:span {:class "sr-only"} "Your Company"]
-        [:img
-         {:class "h-8 w-auto",
-          :src
-          "https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600",
-          :alt ""}]]
+        [:span {:class "sr-only"} "Your Company"]]
        [:button
         {:type "button", :class "-m-2.5 rounded-md p-2.5 text-gray-700"}
         [:span {:class "sr-only"} "Close menu"]
@@ -138,35 +128,45 @@
     [:div
      {:class "mx-auto max-w-2xl py-32 sm:py-48 lg:py-56"}
      [:div
-      {:class "hidden sm:mb-8 sm:flex sm:justify-center"}
-      [:div
-       {:class
-        "relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20"}
-       "Announcing our next round of funding."
-       [:a
-        {:href "#", :class "font-semibold text-indigo-600"}
-        [:span {:class "absolute inset-0", :aria-hidden "true"}]
-        "Read more"
-        [:span {:aria-hidden "true"} "→"]]]]
+      {:class "hidden sm:mb-8 sm:flex sm:justify-center"}]
      [:div
       {:class "text-center"}
       [:h1
        {:class
         "text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl"}
-       "Data to enrich your online business"]
+       "Te amo mi amor"]
       [:p
        {:class
         "mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8"}
-       "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat."]
+       "Te dedico estas dos canciones."]
       [:div
        {:class "mt-10 flex items-center justify-center gap-x-6"}
+       
+       [:div
+        {:class "flex items-center justify-center"}
+        [:audio
+         {:controls "", :class "w-64 rounded-lg shadow-lg"}
+         (if (= (System/getenv "ENVIRONMENT") "prod")
+           [:source {:src "song-1.mp3", :type "audio/mp3"}]
+           [:source {:src "../song-1.mp3", :type "audio/mp3"}])
+         "Your browser does not support the audio element."]]
+       
+       [:div
+        {:class "flex items-center justify-center"}
+        [:audio
+         {:controls "", :class "w-64 rounded-lg shadow-lg"}
+         (if (= (System/getenv "ENVIRONMENT") "prod")
+           [:source {:src "song-2.mp3", :type "audio/mp3"}]
+           [:source {:src "../song-2.mp3", :type "audio/mp3"}])
+         "Your browser does not support the audio element."]]
+
        [:a
         {:href "#",
          :class
-         "rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"}
+         "rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 lg:hidden"}
         "Get started"]
        [:a
-        {:href "#", :class "text-sm/6 font-semibold text-gray-900"}
+        {:href "#", :class "text-sm/6 font-semibold text-gray-900 lg:hidden"}
         "Learn more"
         [:span {:aria-hidden "true"} "→"]]]]]
     [:div
