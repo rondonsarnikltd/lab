ee3bc70abe40eccc65ccca704cb41331df643c19
Yunior Alayo <yunior.alayo@gmail.com>
2025-02-14 10:36:29 +0100

    Added a template to reuse.

--- components/dylan/src/app/dylan/index.clj
+++ components/dylan/src/app/dylan/index.clj
@@ -3,56 +3,178 @@
 (defn content []
   [:div
    {:class "bg-white"}
-   [:div
-    {:class "mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8"}
+   [:header
+    {:class "absolute inset-x-0 top-0 z-50"}
+    [:nav
+     {:class "flex items-center justify-between p-6 lg:px-8",
+      :aria-label "Global"}
+     [:div
+      {:class "flex lg:flex-1"}
+      [:a
+       {:href "#", :class "-m-1.5 p-1.5"}
+       [:span {:class "sr-only"} "Your Company"]
+       [:img
+        {:class "h-8 w-auto",
+         :src
+         "https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600",
+         :alt ""}]]]
+     [:div
+      {:class "flex lg:hidden"}
+      [:button
+       {:type "button",
+        :class
+        "-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"}
+       [:span {:class "sr-only"} "Open main menu"]
+       [:svg
+        {:class "size-6",
+         :fill "none",
+         :viewBox "0 0 24 24",
+         :stroke-width "1.5",
+         :stroke "currentColor",
+         :aria-hidden "true",
+         :data-slot "icon"}
+        [:path
+         {:stroke-linecap "round",
+          :stroke-linejoin "round",
+          :d "M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"}]]]]
+     [:div
+      {:class "hidden lg:flex lg:gap-x-12"}
+      [:a
+       {:href "#", :class "text-sm/6 font-semibold text-gray-900"}
+       "Product"]
+      [:a
+       {:href "#", :class "text-sm/6 font-semibold text-gray-900"}
+       "Features"]
+      [:a
+       {:href "#", :class "text-sm/6 font-semibold text-gray-900"}
+       "Marketplace"]
+      [:a
+       {:href "#", :class "text-sm/6 font-semibold text-gray-900"}
+       "Company"]]
+     [:div
+      {:class "hidden lg:flex lg:flex-1 lg:justify-end"}
+      [:a
+       {:href "#", :class "text-sm/6 font-semibold text-gray-900"}
+       "Log in"
+       [:span {:aria-hidden "true"} "→"]]]]
+    (comment "Mobile menu, show/hide based on menu open state.")
     [:div
-     {:class
-      "relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0"}
-     [:svg
-      {:viewBox "0 0 1024 1024",
-       :class
-       "absolute left-1/2 top-1/2 -z-10 size-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0",
-       :aria-hidden "true"}
-      [:circle
-       {:cx "512",
-        :cy "512",
-        :r "512",
-        :fill "url(#759c1415-0410-454c-8f7c-9a820de03641)",
-        :fill-opacity "0.7"}]
-      [:defs
-       [:radialgradient
-        {:id "759c1415-0410-454c-8f7c-9a820de03641"}
-        [:stop {:stop-color "#7775D6"}]
-        [:stop {:offset "1", :stop-color "#E935C1"}]]]]
+     {:class "lg:hidden", :role "dialog", :aria-modal "true"}
+     (comment
+       "Background backdrop, show/hide based on slide-over state.")
+     [:div {:class "fixed inset-0 z-50"}]
      [:div
       {:class
-       "mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left"}
-      [:h2
-       {:class
-        "text-balance text-3xl font-semibold tracking-tight text-white sm:text-4xl"}
-       "Boost your productivity. Start using our app today."]
-      [:p
-       {:class "mt-6 text-pretty text-lg/8 text-gray-300"}
-       "Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla."]
+       "fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"}
+      [:div
+       {:class "flex items-center justify-between"}
+       [:a
+        {:href "#", :class "-m-1.5 p-1.5"}
+        [:span {:class "sr-only"} "Your Company"]
+        [:img
+         {:class "h-8 w-auto",
+          :src
+          "https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600",
+          :alt ""}]]
+       [:button
+        {:type "button", :class "-m-2.5 rounded-md p-2.5 text-gray-700"}
+        [:span {:class "sr-only"} "Close menu"]
+        [:svg
+         {:class "size-6",
+          :fill "none",
+          :viewBox "0 0 24 24",
+          :stroke-width "1.5",
+          :stroke "currentColor",
+          :aria-hidden "true",
+          :data-slot "icon"}
+         [:path
+          {:stroke-linecap "round",
+           :stroke-linejoin "round",
+           :d "M6 18 18 6M6 6l12 12"}]]]]
+      [:div
+       {:class "mt-6 flow-root"}
+       [:div
+        {:class "-my-6 divide-y divide-gray-500/10"}
+        [:div
+         {:class "space-y-2 py-6"}
+         [:a
+          {:href "#",
+           :class
+           "-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"}
+          "Product"]
+         [:a
+          {:href "#",
+           :class
+           "-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"}
+          "Features"]
+         [:a
+          {:href "#",
+           :class
+           "-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"}
+          "Marketplace"]
+         [:a
+          {:href "#",
+           :class
+           "-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"}
+          "Company"]]
+        [:div
+         {:class "py-6"}
+         [:a
+          {:href "#",
+           :class
+           "-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"}
+          "Log in"]]]]]]]
+   [:div
+    {:class "relative isolate px-6 pt-14 lg:px-8"}
+    [:div
+     {:class
+      "absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80",
+      :aria-hidden "true"}
+     [:div
+      {:class
+       "relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]",
+       :style
+       "clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"}]]
+    [:div
+     {:class "mx-auto max-w-2xl py-32 sm:py-48 lg:py-56"}
+     [:div
+      {:class "hidden sm:mb-8 sm:flex sm:justify-center"}
       [:div
        {:class
-        "mt-10 flex items-center justify-center gap-x-6 lg:justify-start"}
+        "relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20"}
+       "Announcing our next round of funding."
+       [:a
+        {:href "#", :class "font-semibold text-indigo-600"}
+        [:span {:class "absolute inset-0", :aria-hidden "true"}]
+        "Read more"
+        [:span {:aria-hidden "true"} "→"]]]]
+     [:div
+      {:class "text-center"}
+      [:h1
+       {:class
+        "text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl"}
+       "Data to enrich your online business"]
+      [:p
+       {:class
+        "mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8"}
+       "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat."]
+      [:div
+       {:class "mt-10 flex items-center justify-center gap-x-6"}
        [:a
         {:href "#",
          :class
-         "rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"}
+         "rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"}
         "Get started"]
        [:a
-        {:href "#", :class "text-sm/6 font-semibold text-white"}
+        {:href "#", :class "text-sm/6 font-semibold text-gray-900"}
         "Learn more"
-        [:span {:aria-hidden "true"} "→"]]]]
+        [:span {:aria-hidden "true"} "→"]]]]]
+    [:div
+     {:class
+      "absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]",
+      :aria-hidden "true"}
      [:div
-      {:class "relative mt-16 h-80 lg:mt-8"}
-      [:img
-       {:class
-        "absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10",
-        :src
-        "https://tailwindui.com/plus/img/component-images/dark-project-app-screenshot.png",
-        :alt "App screenshot",
-        :width "1824",
-        :height "1080"}]]]]])
+      {:class
+       "relative left-[calc(50%+3rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]",
+       :style
+       "clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"}]]]])
