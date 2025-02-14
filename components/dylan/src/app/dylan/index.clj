c8b21cfa8436e605650cfd3e719f7c3d33a10d17
Yunior Alayo <yunior.alayo@gmail.com>
2025-02-14 11:39:46 +0100

    Removed some elements to fix the mobile version.

--- components/dylan/src/app/dylan/index.clj
+++ components/dylan/src/app/dylan/index.clj
@@ -13,25 +13,7 @@
       [:a
        {:href "#", :class "-m-1.5 p-1.5"}
        [:span {:class "sr-only"} "Your Company"]]]
-     [:div
-      {:class "flex lg:hidden"}
-      [:button
-       {:type "button",
-        :class
-        "-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"}
-       [:span {:class "sr-only"} "Open main menu"]
-       [:svg
-        {:class "size-6",
-         :fill "none",
-         :viewBox "0 0 24 24",
-         :stroke-width "1.5",
-         :stroke "currentColor",
-         :aria-hidden "true",
-         :data-slot "icon"}
-        [:path
-         {:stroke-linecap "round",
-          :stroke-linejoin "round",
-          :d "M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"}]]]]
+     
      [:div
       {:class "hidden lg:flex lg:gap-x-12"}
       [:a
@@ -58,62 +40,7 @@
      (comment
        "Background backdrop, show/hide based on slide-over state.")
      [:div {:class "fixed inset-0 z-50"}]
-     [:div
-      {:class
-       "fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"}
-      [:div
-       {:class "flex items-center justify-between"}
-       [:a
-        {:href "#", :class "-m-1.5 p-1.5"}
-        [:span {:class "sr-only"} "Your Company"]]
-       [:button
-        {:type "button", :class "-m-2.5 rounded-md p-2.5 text-gray-700"}
-        [:span {:class "sr-only"} "Close menu"]
-        [:svg
-         {:class "size-6",
-          :fill "none",
-          :viewBox "0 0 24 24",
-          :stroke-width "1.5",
-          :stroke "currentColor",
-          :aria-hidden "true",
-          :data-slot "icon"}
-         [:path
-          {:stroke-linecap "round",
-           :stroke-linejoin "round",
-           :d "M6 18 18 6M6 6l12 12"}]]]]
-      [:div
-       {:class "mt-6 flow-root"}
-       [:div
-        {:class "-my-6 divide-y divide-gray-500/10"}
-        [:div
-         {:class "space-y-2 py-6"}
-         [:a
-          {:href "#",
-           :class
-           "-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"}
-          "Product"]
-         [:a
-          {:href "#",
-           :class
-           "-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"}
-          "Features"]
-         [:a
-          {:href "#",
-           :class
-           "-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"}
-          "Marketplace"]
-         [:a
-          {:href "#",
-           :class
-           "-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"}
-          "Company"]]
-        [:div
-         {:class "py-6"}
-         [:a
-          {:href "#",
-           :class
-           "-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"}
-          "Log in"]]]]]]]
+     ]]
    [:div
     {:class "relative isolate px-6 pt-14 lg:px-8"}
     [:div
@@ -158,17 +85,7 @@
          (if (= (System/getenv "ENVIRONMENT") "prod")
            [:source {:src "song-2.mp3", :type "audio/mp3"}]
            [:source {:src "../song-2.mp3", :type "audio/mp3"}])
-         "Your browser does not support the audio element."]]
-
-       [:a
-        {:href "#",
-         :class
-         "rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 lg:hidden"}
-        "Get started"]
-       [:a
-        {:href "#", :class "text-sm/6 font-semibold text-gray-900 lg:hidden"}
-        "Learn more"
-        [:span {:aria-hidden "true"} "→"]]]]]
+         "Your browser does not support the audio element."]]]]]
     [:div
      {:class
       "absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]",
